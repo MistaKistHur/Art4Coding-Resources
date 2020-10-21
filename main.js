16f6eb0933c5e5c83dbe445b101fbdaf28fa70e3
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-10-21 21:23:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 "use-strict";
-	let document = docu;
+	//let document = docu;
 	console.log("Js File Loaded Successfully");
 
-	docu.addEventListener("DOMContentLoaded",(e)=>{
+	document.addEventListener("DOMContentLoaded",(e)=>{
 		  let bodi = doc.querySelector("body");
 		  let header = doc.querySelector("header");
 		  let xhr_video = doc.querySelector(".xhr_video_request");
 
-
+	console.log("DomC Ran Successfully");
 		  xhr_video.addEventListener((e) => {
 		    // fetch extra videos, bump others out of view..
 		  });
