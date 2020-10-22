b5ef7154d6c52adc4e8f5b563149762e2f078e16
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-10-22 19:24:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 "use-strict";
   //let e = e || evt || event;
-  //x
-
-
-
-
-	//
 	console.log("Js File Loaded Successfully");
 
 	document.addEventListener("DOMContentLoaded",(e)=>{
+		console.log("DomC Ran Successfully");
+		    let tick_one = performance.now();
+
+
 		let docu = document;
 		  let bodi = docu.querySelector("body");
 		  let header = docu.querySelector("header");
 		  let xhr_video = docu.querySelector(".xhr_video_request");
 
-	console.log("DomC Ran Successfully");
 
-	
+
+
 		  xhr_video.addEventListener((e) => {
 		    // fetch extra videos, bump others out of view..
 		  });
-	})
+	}) /* ::>> Closing Tag for the MAIN body DOMC loaded event */
+
+
+/* ::>> Use PreventDefault on all top Nav a links then use Ajax/xhr to load in other content .... */
+		document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+		         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+		         event.preventDefault();
+		}, false);
