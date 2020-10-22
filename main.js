7ea11e2d47b4148f6b020522f3b8c3d3c26196d4
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-10-22 15:20:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,18 @@
 
 
 
-	//let document = docu;
+	//
 	console.log("Js File Loaded Successfully");
 
 	document.addEventListener("DOMContentLoaded",(e)=>{
-		  let bodi = doc.querySelector("body");
-		  let header = doc.querySelector("header");
-		  let xhr_video = doc.querySelector(".xhr_video_request");
+		let docu = document;
+		  let bodi = docu.querySelector("body");
+		  let header = docu.querySelector("header");
+		  let xhr_video = docu.querySelector(".xhr_video_request");
 
 	console.log("DomC Ran Successfully");
+
+	
 		  xhr_video.addEventListener((e) => {
 		    // fetch extra videos, bump others out of view..
 		  });
