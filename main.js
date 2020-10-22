1f87d66506c7d814d31992fa52649c2b15f70b5d
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-10-22 19:27:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@
 
 
 /* ::>> Use PreventDefault on all top Nav a links then use Ajax/xhr to load in other content .... */
-		document.querySelector("#id-checkbox").addEventListener("click", function(event) {
-		         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
-		         event.preventDefault();
-		}, false);
+		//document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+		//         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+		//         event.preventDefault();
+		//}, false);
