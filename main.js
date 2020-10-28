42bb617e79572155cc48932356e38aaaf90314d3
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-10-28 00:04:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@
            Consider using parallax aswell... */
         // let h1Anime = gsap.to();  // Flame/Smoke reveal or not? fad in/ slide in?
         // let h2Anime = gsap.to();  // Fade in.
-        let RevolverMapsGsap = gsap.fromTo(".r_maps_container", {opacity: 0}, {opacity: 0.5, duration: 1});
+        let RevolverMapsGsap = gsap.fromTo(".r_maps_container", {opacity: 0}, {opacity: 0.9, duration: 70});
 
         // let navMenu = gsap.to();
 
