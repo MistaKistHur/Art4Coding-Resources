c1b6a0f05b0aa41859fb47b45db278e32c8297e9
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-10-29 22:44:04 +0000

    Update main.js

--- main.js
+++ main.js
@@ -19,11 +19,12 @@
 	// ::>> Page Element Selectors <<:: ......................................................................................
 	// ::>> Worker Scripts <<:: ..............................................................................................
 			if(window.Worker){
-				const AnimeWorker = new Worker('anime_worker.js');
+				const AnimeWorker       = new Worker('anime_worker.js');
+				const InteractiveWorker = new Worker('page_interactions.js');
 				// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Functions_and_classes_available_to_workers
 			}
 			if(window.Worklet){
-
+				// Paint Worklets
 			}
 	// ::>> Worker Scripts <<:: ..............................................................................................
 	// ::>> Media Querys Section <<:: ........................................................................................
