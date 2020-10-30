c2805c02affeb9e6cb89503cc067c63bdce3ac42
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-10-30 02:40:18 +0000

    Update main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+"use-strict";
+
+	document.addEventListener("DOMContentLoaded",(e)=>{
+				  console.log("Js File Loaded Successfully");
+				  console.log("DomC Ran Successfully");
+				    const t0 = performance.now(); // Testing only.
+				    console.log(`::>>${t0}`);
+				    console.log(`::>>Test AB Ref:: X678`);
+
+		const Head          = document.querySelector("head");
+		  let sts           = document.styleSheets;
+		const Bodi          = document.querySelector("body");
+		const BodyObjectArr = document.all;
+		const Header     = document.querySelector("header");
+		const Footer     = document.querySelector("footer");
+		const TopNavLinx = document.getElementById("TopNavLinx");
+
+		Bodi.style.display ="block !important";
+	});
