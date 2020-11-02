e2188eebb95569f4db5529c3beb910168ec3afb1
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-02 22:15:15 +0000

    x

--- main.js
+++ main.js
@@ -2,17 +2,23 @@
 document.body.style.visibility = "visible";
     // document.crypto.KeyObject(type, handle);
 	document.addEventListener("DOMContentLoaded",(e)=>{
+			 console.log("Js File Loaded Successfully");
+			 console.log("DomC Ran Successfully");
+
 		let isDevice = (/android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()));
+		const Head   = document.querySelector("head");
+		  let sts    = document.styleSheets;
+		  if(isDevice){
+		  	// Use Mobile/Tablet Stylesheet & Media Query's
+		  }
 
 
-				  console.log("Js File Loaded Successfully");
-				  console.log("DomC Ran Successfully");
 				    const t0 = performance.now(); // Testing only.
 				    console.log(`::>>${t0}`);
 				    console.log(`::>>Test AB Ref:: X678`);
 
-		const Head          = document.querySelector("head");
-		  let sts           = document.styleSheets;
+
+
 		const Bodi          = document.querySelector("body");
 		const BodyObjectArr = document.all;
 		const Header     = document.querySelector("header");
