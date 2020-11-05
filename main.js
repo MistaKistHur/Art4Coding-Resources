02d51363f3ef46aff9cee4b7bf4388750315b78b
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-05 10:35:47 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 "use-strict";
 // ::>> Create node.js App & use modules mocha & chai for unit testing...
 document.body.style.visibility = "visible";
+	console.log("::>> Js File Loaded Successfully");
     // ::>> document.crypto.KeyObject(type, handle);
 	document.addEventListener("DOMContentLoaded",(e)=>{
-			 console.log("::>> Js File Loaded Successfully");
-			 console.log("::>> DomC Ran Successfully");
+		console.log("::>> DomC Ran Successfully");
+		document.body.setAttribute("token", "x");
+
+		const Bodi       = document.querySelector("body");
+		const Head       = document.querySelector("head");
+		const Header     = document.querySelector("header");
+		const Footer     = document.querySelector("footer");
+		const TopNavLinx = document.getElementById("TopNavLinx");
+		      let sts    = document.styleSheets;
+		      const BodyObjectArr = document.all;
+		// ::>> V:: this is not executing?.........
+		Bodi.style.display ="block !important";
 
 		let isDevice = (/android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent.toLowerCase()));
-		const Head   = document.querySelector("head");
-		  let sts    = document.styleSheets;
+
 		  if(isDevice){
 		  	// ::>> Use Mobile/Tablet Stylesheet & Media Query's..
 		  	console.log(`portable device in use.`);
@@ -20,16 +30,7 @@ document.body.style.visibility = "visible";
 				    console.log(`::>>${t0}`);
 				    console.log(`::>>Test AB Ref:: X678` + console.trace());
 
-
-
-		const Bodi          = document.querySelector("body");
-		const BodyObjectArr = document.all;
-		const Header     = document.querySelector("header");
-		const Footer     = document.querySelector("footer");
-		const TopNavLinx = document.getElementById("TopNavLinx");
-
-		Bodi.style.display ="block !important";
-	});
+	}); //::>> Closing Tag for the main DOMC event listener. ...................
 
 	/* ::>> debugging.
 			debugger;
