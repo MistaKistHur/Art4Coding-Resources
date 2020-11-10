d03ae282938341150eaf456ffc7f52f46c089411
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-10 00:49:25 +0000

    Adding wikiart api

---
+++ Modules/Access.js
@@ -0,0 +1,4 @@
+let WikiArtKey = ``;
+let WikiArtValue = ``;
+// https://stackoverflow.com/questions/1983990/how-to-open-source-an-application-that-uses-api-keys
+// https://stackoverflow.com/questions/35137838/how-to-protect-google-api-key-in-an-open-source-github-heroku-application
---
+++ Scripts/init.js
@@ -0,0 +1,2 @@
+"use-strict";
+ require("/Modules/Access.js");
