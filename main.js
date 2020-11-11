db21fd3c70d1d0106bf334552faf76d61a64dd85
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-11 05:41:11 +0000

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@
 		    console.log(nav_arr);
 		    nav_arr.addEventListener('mousedown',(e) => {
 		  			// Todo...
-		  		let cs = window.getComputedStyle(HidNav, null);
+		  		let cs = window.getComputedStyle("HidNav", null).getPropertyValue("transform");;
 		  		console.log(cs);
 
 
