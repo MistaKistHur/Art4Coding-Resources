3480635a76f0b62428c80907cf55bd8f658e1f64
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2020-11-28 00:52:11 +0000

    Update module.sidebar.js

---
+++ Modules/module.sidebar.js
@@ -0,0 +1,13 @@
+export default class{
+	constructor(){
+		// create a sidebar, that is a modal slide in element.
+		this.style      = {};
+		this.background = "";
+		this.links      = [];
+		this.btns       = [];
+		this.actions    = function(){
+
+		}
+		this.
+	}
+}
